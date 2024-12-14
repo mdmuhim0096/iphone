d45b12d2488274b23bdf81e61535b135d260bcfa
muhim <muhinking765@gmail.com>
2024-12-14 19:08:51 +0600

    jjj

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/iphone_15/",
+  base: "/apple_iphone_15/",
   build: {
     sourcemap: true
   }
