bea4b4845781b3ccfa30bac8330243076405c17e
muhim <muhinking765@gmail.com>
2024-12-14 18:30:13 +0600

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/iphone/",
+  base: "/iphone_15/",
   build: {
     sourcemap: true
   }
